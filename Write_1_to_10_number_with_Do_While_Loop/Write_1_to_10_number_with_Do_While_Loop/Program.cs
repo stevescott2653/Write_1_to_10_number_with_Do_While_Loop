85e258698d05e1a769a5f756fb9649a1273192b9
Steven Scott <stevescott2653@gmail.com>
2019-09-25 16:03:03 -0400

    Add files via upload

---
+++ Write_1_to_10_number_with_Do_While_Loop/Write_1_to_10_number_with_Do_While_Loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_1_to_10_number_with_Do_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i <= 10);
+        }
+    }
+}
